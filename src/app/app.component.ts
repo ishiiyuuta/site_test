546f481fe85e6b9f79994ba095aa8a920d800eec
Rahul Sharma <rahu8299@gmail.com>
2020-09-06 11:07:18 +0530

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
